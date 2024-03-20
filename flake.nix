2c056e20db02aa4760b1b22c3349359293e6af2c
Peter Trotman <petertrotman@gmail.com>
2024-03-20 14:41:04 +0000

    feat(dev): Add nix direnv for development

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ fenix.overlays.default ]; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [
+            (pkgs.fenix.fromToolchainFile {
+              dir = ./.;
+              sha256 = "sha256-U2yfueFohJHjif7anmJB5vZbpP7G6bICH4ZsjtufRoU=";
+            })
+          ];
+        };
+      });
+}
